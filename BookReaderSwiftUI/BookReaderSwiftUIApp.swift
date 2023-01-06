6f3fc282cc23afeb6ad74753eac29984fce52fa7
Omar Tharwat <77683787+omartharwat74@users.noreply.github.com>
2023-01-06 06:37:54 +0200

    Initial Commit

---
+++ BookReaderSwiftUI/BookReaderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderSwiftUIApp.swift
+//  BookReaderSwiftUI
+//
+//  Created by Omar Tharwat on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookReaderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
